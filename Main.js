328f68e45214fa4182f792af9bd4fab32c48764a
ronnie771 <ronnie.michayev@gmail.com>
2021-04-23 18:34:08 +0300

    rm modules

--- Main.js
+++ Main.js
@@ -9,7 +9,6 @@ const mongoose = require('mongoose');
 const path = require('path');
 var bodyParser = require('body-parser');
 
-mongodb+srv://atlasron:ronm56@cluster0.dmdmk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my_rest_api', {
   useNewUrlParser: true,

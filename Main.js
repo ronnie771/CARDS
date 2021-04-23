f081318ac956389a8d635f3553b38bebda4e36b6
ronnie771 <ronnie.michayev@gmail.com>
2021-04-23 16:45:26 +0300

    v1

---
+++ Main.js
@@ -0,0 +1,39 @@
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const cards = require('./routes/cards');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const mongoose = require('mongoose');
+const path = require('path');
+var bodyParser = require('body-parser');
+
+
+
+mongoose.connect('mongodb://localhost/my_rest_api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+  
+  app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+  app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.send(path.join(__dirname, 'build', 'index.html'))
+});
+ 
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/cards', cards);
+
+
+
+
+const port = process.env.PORT || 3000;
+http.listen(port, () => console.log(`Listening on port ${port}...`));

2c40b29fa3b9baf980518e8cd606c0ff3e06befb
ronnie mich <ronnie.michayev@gmail.com>
2021-09-04 01:25:44 +0300

    v2

--- Main.js
+++ Main.js
@@ -31,10 +31,10 @@ app.use(cors())
 
 
 
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static('build'));
 
   
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 });
 

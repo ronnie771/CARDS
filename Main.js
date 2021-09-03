29cc13319e72e862aa287a277e5f38c242c7b167
ronnie mich <ronnie.michayev@gmail.com>
2021-09-04 01:19:21 +0300

    v2

--- Main.js
+++ Main.js
@@ -32,11 +32,12 @@ app.use(cors())
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, 'build')));
-}
+
   
 app.get('*', (req, res) => {
-  res.send(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 });
+}
  
 
 app.use('/users', users);

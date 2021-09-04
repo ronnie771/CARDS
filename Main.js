d0531e1ef27814859c24ff59b7b6af15009c4afe
ronnie mich <ronnie.michayev@gmail.com>
2021-09-04 05:51:57 +0300

    v2

--- Main.js
+++ Main.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production'){
  
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build' ,'index.html'))
+  res.send(path.join(__dirname, 'build' ,'index.html'))
 }); 
 }
 
